b41648aa55b6255d82ec81d48bcc115f52f869af
crisncris0000 <christopherrivera384@gmail.com>
2023-10-21 13:05:26 -0400

    Converting tic tac toe to react

---
+++ App.js
@@ -0,0 +1,26 @@
+export function App() {
+    return (
+        <div>
+            <h1>Tic-React-Toe</h1>
+            <h1>Tic-Tac-Toe</h1>
+
+            <h2>It's X's turn!</h2>
+
+            <div class="flex-container flex-column">
+                <div class="flex-container flex-wrap" id="board">
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                    <div class="square"></div>
+                </div>
+
+                <button id="reset-button">Reset</button>
+            </div>
+        </div>
+    )
+}
