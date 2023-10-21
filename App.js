4272e0bc7d21d4468535b5d8385e14a0a8e40278
crisncris0000 <christopherrivera384@gmail.com>
2023-10-21 14:17:10 -0400

    Redo event logic to work with react components

--- App.js
+++ App.js
@@ -5,27 +5,46 @@ const winningCombos = [
 ];
 
 function App() {
-    const board = ["", "", "", "", "", "", "", "", ""];
-    const turn = "X";
-    const gameOver = false;
-
+    const [board, setBoard] = React.useState(["","","","","","","","",""]);
+    const [turn, setTurn] = React.useState("X");
+    const [gameOver, setGameOver] = React.useState(false);
+    const [win, setWin] = React.useState(null);
 
     function handleTurn(event) {
-        console.log(event.target, event.target.id)
-        let idx = event.target.id
-        if (gameOver == false) {
-          board[idx] = turn
-          turn = turn === 'X' ? 'O' : 'X'
-          //  win = getWinner()
-          // render()
+        const idx = parseInt(event.target.id, 10);
+        if (!gameOver && board[idx] === "") {
+            const newBoard = [...board];
+            newBoard[idx] = turn;
+            setBoard(newBoard);
+            setTurn(turn === 'X' ? 'O' : 'X');
+            checkForWinner(newBoard);
         }
-      }
+    }
+
+    function checkForWinner(currentBoard) {
+        for (const combo of winningCombos) {
+            const [a, b, c] = combo;
+            if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
+                setWin(currentBoard[a]);
+                setGameOver(true);
+                return;
+            }
+        }
+        if (currentBoard.every(square => square !== "")) {
+            setWin('Draw');
+            setGameOver(true);
+        }
+    }
 
     return (
         <div>
             <h1>Tic-React-Toe</h1>
             <h1>Tic-Tac-Toe</h1>
-            <h2>It's {turn}'s turn!</h2>
+            {gameOver ? (
+                <h2>{win ? `${win} wins!` : 'It\'s a draw!'}</h2>
+            ) : (
+                <h2>It's {turn}'s turn!</h2>
+            )}
 
             <div className="flex-container flex-column">
                 <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
@@ -40,6 +59,7 @@ function App() {
                     setBoard(["", "", "", "", "", "", "", "", ""]);
                     setTurn('X');
                     setGameOver(false);
+                    setWin(null);
                 }}>
                     Reset
                 </button>
